06f03ff66f4771da76a2101f0bbc53fb9999daec
greenbean-tf <Leo2004510@gmail.com>
2025-05-31 22:06:37 +0800

    Add files via upload

--- random_forest.py
+++ random_forest.py
@@ -103,8 +103,8 @@ print(summary.tail(5))
 # print(results_df[results_df["Ticker"] == "AAPL"])
 
 # Optional: plot one stock result
-sample_stock = np.random.choice(results_df['Ticker'].values)  # random
-#sample_stock = results_df.sort_values('R2').iloc[502]['Ticker'] #highest R-square
+#sample_stock = np.random.choice(results_df['Ticker'].values)  # random
+sample_stock = results_df.sort_values('R2').iloc[502]['Ticker'] #highest R-square
 #sample_stock = results_df.sort_values('R2').iloc[0]['Ticker'] #lowest R-square
 #sample_stock = "AAPL"
 sample_df = df[df['Name'] == sample_stock].copy()
